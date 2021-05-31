36c28226d4458d678a69df51f59bf3fadff6a671
_uchanlee <uchanlee.dev@gmail.com>
2021-05-31 21:47:30 +0900

    엔트리 인잇 (#1)

--- projects.ts
+++ projects.ts
@@ -12,4 +12,15 @@ export const projects: Project[] = [
     thumbnail_url: 'https://user-images.githubusercontent.com/48552260/114111695-99e1b100-9915-11eb-8396-8b6e9344a9b9.gif',
     created_at: 'Mon May 31 2021 20:13:38 GMT+0900 (Korean Standard Time)',
   },
+  {
+    id: 2,
+    project_name: '엔트리 인잇',
+    description: '대덕소프트웨어마이스터고등학교 커뮤니티입니다.',
+    owner: 'EntryDSM',
+    thumbnail_url: 'https://avatars.githubusercontent.com/u/38097893?s=200&v=4',
+    play_link: 'https://init.entrydsm.hs.kr',
+    user: 'EntryDSM',
+    repo: 'Persian',
+    created_at: 'Mon May 31 2021 21:39:06 GMT+0900 (Korean Standard Time)',
+  },
 ];
